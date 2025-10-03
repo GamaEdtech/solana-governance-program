2f959efd0b1d14f474e633a5650eecc8a1cd8c44
Sadeq Naderi <sadeq.naderi@gmail.com>
2025-10-03 16:12:33 +0330

    feat(security): enable security file for project

--- programs/gamaedtech-program/src/lib.rs
+++ programs/gamaedtech-program/src/lib.rs
@@ -1,11 +1,24 @@
 use anchor_lang::prelude::*;
-
 declare_id!("9F77hJsRRXs7vF9UDncZKth2r5wEPgcRkEfyoZDNQ3eK");
 
 use instructions::*;
 mod error;
 mod instructions;
 mod state;
+
+#[cfg(not(feature = "no-entrypoint"))]
+use solana_security_txt::security_txt;
+
+#[cfg(not(feature = "no-entrypoint"))]
+security_txt! {
+    name: "GamaEdtech Governance",
+    project_url: "https://gamatrain.com",
+    contacts: "security@gamatrain.com",
+    policy: "https://gamatrain.com/security/policy",
+    preferred_languages: "en",
+    source_code: "https://github.com/GamaEdtech/solana-governance-program"
+}
+
 #[program]
 pub mod gamaedtech_program {
 

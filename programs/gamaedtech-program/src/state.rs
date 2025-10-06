e84fd11677819361417c43cfea81a262a43cd50c
Sadeq Naderi <sadeq.naderi@gmail.com>
2025-10-06 13:50:39 +0330

    feat(proposal): update proposal account to use PDA

--- programs/gamaedtech-program/src/state.rs
+++ programs/gamaedtech-program/src/state.rs
@@ -13,6 +13,25 @@ pub struct Proposal {
     pub expires_at: i64, // Expiration timestamp in seconds
 }
 
+impl Space for Proposal {
+    const INIT_SPACE: usize = 32 +        // owner
+        (4 + 100) + // title
+        (4 + 800) + // brief
+        (4 + 50) +  // cate
+        (4 + 200) + // reference
+        8 +         // amount
+        8 +         // agree_votes
+        8 +         // disagree_votes
+        8 +         // created_at
+        8; // expires_at
+}
+
+#[account]
+pub struct UserState {
+    pub user: Pubkey,
+    pub proposal_count: u64,
+}
+
 #[account]
 pub struct VoteRecord {
     pub proposal_id: Pubkey, // Linked proposal ID
